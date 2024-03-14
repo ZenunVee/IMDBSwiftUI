16962234ba2f81fd6db1fb76850e207e7bf858dd
Zenun Vucetovic <zvucetovic@leedsllc.com>
2024-03-13 20:40:52 -0400

    Initial Commit

---
+++ TinyBigMovie/TinyBigMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinyBigMovieApp.swift
+//  TinyBigMovie
+//
+//  Created by Zenun Vucetovic on 3/13/24.
+//
+
+import SwiftUI
+
+@main
+struct TinyBigMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
